f88f39fc768c736fcd3e950f36712cdf84ce990a
Greyson Kirk <greysonkirk@yahoo.com>
2021-01-14 11:33:50 -0600

    models

---
+++ client/src/pages/login.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function login() {
+  return (
+    <Form>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+      <Form.Group>
+        <Form.Label htmlFor="inputPassword6">Password</Form.Label>
+        <Form.Control
+          type="password"
+          className="mx-sm-3"
+          id="inputPassword6"
+          aria-describedby="passwordHelpInline"
+        />
+        <Form.Text id="passwordHelpBlock" muted>
+          Must be 8-20 characters long.
+        </Form.Text>
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
